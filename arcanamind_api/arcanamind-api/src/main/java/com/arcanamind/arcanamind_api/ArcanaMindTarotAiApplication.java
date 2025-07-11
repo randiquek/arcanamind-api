66e87ca71bc086bc2240195890d5a983e3c97378
Randi K <randiquek@gmail.com>
2025-07-11 18:08:27 -0400

    Added Spring Boot base project files

---
+++ arcanamind_api/arcanamind-api/src/main/java/com/arcanamind/arcanamind_api/ArcanaMindTarotAiApplication.java
@@ -0,0 +1,13 @@
+package com.arcanamind.arcanamind_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcanaMindTarotAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcanaMindTarotAiApplication.class, args);
+	}
+
+}
---
+++ arcanamind_api/arcanamind-api/src/test/java/com/arcanamind/arcanamind_api/ArcanaMindTarotAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcanamind.arcanamind_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcanaMindTarotAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
